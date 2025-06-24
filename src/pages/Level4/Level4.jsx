8b1c92e7f4fcdfe875e816201680be17f648416f
LAPTOP-1L70EI88\o_tsubasa <tubasan2005@gmail.com>
2025-06-24 19:01:27 +0900

    level4

--- src/pages/Level4/Level4.jsx
+++ src/pages/Level4/Level4.jsx
@@ -1,14 +1,27 @@
 import { useNavigate } from 'react-router-dom'
 import './Level4.css'
-
+import Header from '../../components/Header.jsx'
 function Level4() {
   const navigate = useNavigate()
-
+   const labels = ['あ', 'い', 'う']
   return (
-    <div className="center-container">
-      <button onClick={() => navigate('/Level5')}>
-        level5
-      </button>
+    <div>
+      <Header level={4} />
+      <div className='right-bottom-button'>
+        <button onClick={() => navigate('/Level3')}>
+          level3
+        </button>
+      </div>
+      <div className="vertical-buttons">
+        {labels.map((label, index) => (
+          <button key={index}>{label}</button>
+        ))}
+      </div>
+      <div className='left-bottom-button'>
+        <button onClick={() => navigate('/Level5')}>
+          level5
+        </button>
+      </div>
     </div>
   )
 }
